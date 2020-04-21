4c7f98ca133587c6081e73e83963fc77596361cb
Brian Brown II <bkeithbii@gmail.com>
2020-04-21 14:14:52 -0400

    Contact form - email test

--- form-to-email.php
+++ form-to-email.php
@@ -1,16 +1,24 @@
 <?php
-if(!isset($_POST['submit'])) {
+
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "bkbrown.dev@gmail.com";
-    $headers = "From: ".$visitor_email;
-    $txt = "You have received an email from".$name.".\n\n".$message;
+    $email_from = 'bkeithbii@gmail.com';
+    $email_subject = "New Form Submission";
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
 
-    mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend");
-}
+    $to = "bkbrown.dev@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
