d08fab54a2c7a1e9c132e454213ba1413a52ac50
Brian Brown II <bkeithbii@gmail.com>
2020-04-22 18:10:20 -0400

    Header updates 1.0

--- script.js
+++ script.js
@@ -97,3 +97,29 @@ $(document).ready(function () {
     }
   });
 });
+
+// Updating header functionality
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+};
+
+// Navbar links
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a");
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+});
