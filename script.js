0c15b40a8cf11dcb9b94cd9b028ca4c067d9e8c3
Brian Brown II <bkeithbii@gmail.com>
2020-04-27 16:05:54 -0400

    Portfolio 2.0 (design and content changes)

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 // Gallery item filter
 
-const filterButtons = document.querySelector("#filter-butts").children;
-const items = document.querySelector(".projects-gallery").children;
+// const filterButtons = document.querySelector("#filter-butts").children;
+// const items = document.querySelector(".projects-gallery").children;
 
-for (let i = 0; i < filterButtons.length; i++) {
-  filterButtons[i].addEventListener("click", function () {
-    for (let j = 0; j < filterButtons.length; j++) {
-      filterButtons[j].classList.remove("active");
-    }
-    this.classList.add("active");
-    const target = this.getAttribute("data-target");
+// for (let i = 0; i < filterButtons.length; i++) {
+//   filterButtons[i].addEventListener("click", function () {
+//     for (let j = 0; j < filterButtons.length; j++) {
+//       filterButtons[j].classList.remove("active");
+//     }
+//     this.classList.add("active");
+//     const target = this.getAttribute("data-target");
 
-    for (let k = 0; k < items.length; k++) {
-      items[k].style.display = "none";
-      if (target == items[k].getAttribute("data-id")) {
-        // items[k].style.flex = "0 0 45%";
-        // items[k].style.maxWidth = "45%";
-        items[k].style.display = "flex";
-        items[k].style.flexWrap = "wrap";
-        items[k].style.flexDirection = "row";
-        items[k].style.justifyContent = "center";
-        items[k].style.padding = "20px";
-      }
-      if (target == "all") {
-        items[k].style.flex = "0 0 calc(33.33% - 5px)";
-        items[k].style.maxWidth = "calc(33.33% - 5px)";
-        items[k].style.display = "flex";
-        items[k].style.flexWrap = "wrap";
-        items[k].style.justifyContent = "space-between";
-      }
-    }
-  });
-}
+//     for (let k = 0; k < items.length; k++) {
+//       items[k].style.display = "none";
+//       if (target == items[k].getAttribute("data-id")) {
+//         // items[k].style.flex = "0 0 45%";
+//         // items[k].style.maxWidth = "45%";
+//         items[k].style.display = "flex";
+//         items[k].style.flexDirection = "row";
+//         items[k].style.justifyContent = "center";
+//         // items[k].style.padding = "10px";
+//       }
+//       if (target == "all") {
+//         items[k].style.display = "flex";
+//         // items[k].style.flexWrap = "wrap";
+//         items[k].style.flexDirection = "row";
+//         items[k].style.justifyContent = "center";
+//         items[k].style.height = "250px";
+//         items[k].style.width = "300px";
+//       }
+//     }
+//   });
+// }
 
 // Modal
 
-const closeModal = document.querySelector(".close-modal");
-const modal = document.querySelector(".modal");
-const modalImage = modal.querySelector("img");
+// const closeModal = document.querySelector(".close-modal");
+// const modal = document.querySelector(".modal");
+// const modalImage = modal.querySelector("img");
 
-modal.addEventListener("click", function () {
-  if (event.target != modalImage) {
-    modal.classList.add("hide");
-    modal.classList.remove("show");
-  }
-});
+// modal.addEventListener("click", function () {
+//   if (event.target != modalImage) {
+//     modal.classList.add("hide");
+//     modal.classList.remove("show");
+//   }
+// });
 
-closeModal.addEventListener("click", function () {
-  modal.classList.add("hide");
-  modal.classList.remove("show");
-});
+// closeModal.addEventListener("click", function () {
+//   modal.classList.add("hide");
+//   modal.classList.remove("show");
+// });
 
-const gallery = document.querySelector(".projects-gallery");
-const galleryItem = gallery.querySelectorAll(".item");
+// const gallery = document.querySelector(".projects-gallery");
+// const galleryItem = gallery.querySelectorAll(".item");
 
-galleryItem.forEach(function (element) {
-  element
-    .querySelector(".fa-search-plus")
-    .addEventListener("click", function () {
-      modal.classList.remove("hide");
-      modal.classList.add("show");
-      modalImage.src = element.querySelector("img").getAttribute("src");
-    });
-});
+// galleryItem.forEach(function (element) {
+//   element
+//     .querySelector(".fa-search-plus")
+//     .addEventListener("click", function () {
+//       modal.classList.remove("hide");
+//       modal.classList.add("show");
+//       modalImage.src = element.querySelector("img").getAttribute("src");
+//     });
+// });
 
 // Contact Form
 $(document).ready(function () {
